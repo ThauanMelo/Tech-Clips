52f3453a1339b6e453d74959f338854108b9190d
ThauanMelo <thauanamz@gmail.com>
2023-03-27 11:49:19 -0300

    add searchbar

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -42,3 +42,35 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li"
 );
 mobileNavbar.init();
+
+//Search bar
+
+const input = document.querySelector(".search-input");
+const videos = document.querySelectorAll(".card-item");
+
+function searchVideos(filter) {
+  [].forEach.call(videos, function (video) {
+    const title = video.getElementsByTagName("p")[0].textContent.toLowerCase();
+    const iframe = video.getElementsByTagName("iframe")[0];
+    const videoId = iframe.src.split("/").pop();
+
+    // obtém o ID do vídeo do link do iframe
+
+    if (title.indexOf(filter) > -1) {
+      video.style.display = "";
+      iframe.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+
+      // adiciona o parâmetro autoplay
+    } else {
+      video.style.display = "none";
+      iframe.src = "https://www.youtube.com/embed/" + videoId;
+      // remove o parâmetro autoplay
+    }
+  });
+}
+
+input.addEventListener("keyup", (event) => {
+  const searchValue = event.target.value.toLowerCase();
+
+  searchVideos(searchValue);
+});
